ericwu17/trainyard

commit a13cdc57a613a25b81829698f2b1bc0848010b9c

remove useless line of code

src/ui/level/mod.rs
@@ -3,7 +3,7 @@ pub mod speed_slider;
 pub mod status_text;
 
 use bevy::prelude::*;
-use speed_slider::{spawn_speed_slider, TrainSpeed};
+use speed_slider::spawn_speed_slider;
 use status_text::update_status_text;
 
 use crate::level::LevelState;
@@ -47,7 +47,6 @@ fn spawn_level_ui(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     ui_root_query: Query<Entity, With<super::UIRootContainer>>,
-    mut train_speed: ResMut<TrainSpeed>,
 ) {
     let ui_root = ui_root_query.single();
     let font: Handle<Font> = asset_server.load("fonts/kenyan_coffee_rg.otf");
@@ -175,7 +174,6 @@ fn spawn_level_ui(
         .id();
     let status_text_box = commands.spawn(status_text_box).id();
     let slider = spawn_speed_slider(&mut commands, font);
-    train_speed.0 = train_speed.0;
     let status_text = commands.spawn((status_text, LevelStatusText)).id();
 
     commands.entity(ui_root).push_children(&[level_root]);

commit fce9dc654f699acd206402eed0184ddf0c1e1345

fix bug that speed slider was starting at zero
the bug was introduced by a13cdc5

src/ui/level/mod.rs
@@ -3,7 +3,7 @@ pub mod speed_slider;
 pub mod status_text;
 
 use bevy::prelude::*;
-use speed_slider::spawn_speed_slider;
+use speed_slider::{spawn_speed_slider, TrainSpeed};
 use status_text::update_status_text;
 
 use crate::level::LevelState;
@@ -47,6 +47,7 @@ fn spawn_level_ui(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     ui_root_query: Query<Entity, With<super::UIRootContainer>>,
+    train_speed: Res<TrainSpeed>,
 ) {
     let ui_root = ui_root_query.single();
     let font: Handle<Font> = asset_server.load("fonts/kenyan_coffee_rg.otf");
@@ -173,7 +174,7 @@ fn spawn_level_ui(
         .spawn((canvas_placeholder, YardPlaceholderNode))
         .id();
     let status_text_box = commands.spawn(status_text_box).id();
-    let slider = spawn_speed_slider(&mut commands, font);
+    let slider = spawn_speed_slider(&mut commands, font, &train_speed);
     let status_text = commands.spawn((status_text, LevelStatusText)).id();
 
     commands.entity(ui_root).push_children(&[level_root]);

src/ui/level/speed_slider.rs
@@ -37,7 +37,11 @@ impl Plugin for SpeedSliderPlugin {
     }
 }
 
-pub fn spawn_speed_slider(commands: &mut Commands, font: Handle<Font>) -> Entity {
+pub fn spawn_speed_slider(
+    commands: &mut Commands,
+    font: Handle<Font>,
+    current_train_speed: &TrainSpeed,
+) -> Entity {
     let slider_button_width = SLIDER_BUTTON_WIDTH;
     let slider_width = super::BUTTON_WIDTH;
     let slider_height = 30.0;
@@ -65,7 +69,7 @@ pub fn spawn_speed_slider(commands: &mut Commands, font: Handle<Font>) -> Entity
     let slider_spacer = (
         NodeBundle {
             style: Style {
-                width: Val::Px(0.0),
+                width: Val::Px(calculate_spacer_size(current_train_speed)),
                 height: Val::Percent(100.0),
                 ..default()
             },
@@ -165,12 +169,15 @@ fn update_speed_slider_position(
     speed: Res<TrainSpeed>,
     mut spacer_query: Query<&mut Style, With<SpeedSliderSpacer>>,
 ) {
-    let slider_border_size = 3.0;
-
-    let new_spacer_size =
-        speed.0 * (super::BUTTON_WIDTH - SLIDER_BUTTON_WIDTH - 2.0 * slider_border_size);
+    let new_spacer_size = calculate_spacer_size(&speed);
 
     for mut spacer_style in spacer_query.iter_mut() {
         spacer_style.width = Val::Px(new_spacer_size);
     }
+}
+
+fn calculate_spacer_size(speed: &TrainSpeed) -> f32 {
+    let slider_border_size = 3.0;
+
+    speed.0 * (super::BUTTON_WIDTH - SLIDER_BUTTON_WIDTH - 2.0 * slider_border_size)
 }